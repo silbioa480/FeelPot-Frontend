c019f52b24679b76270d5781c38c2042e7a660b0
silbioa480 <silbioa480@gmail.com>
2022-01-17 16:35:02 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <GlobalStyle />
       <Router />
-      <ReactQueryDevtools initialIsOpen={true} />
+      {/* <ReactQueryDevtools initialIsOpen={true} /> */}
     </>
   );
 }
