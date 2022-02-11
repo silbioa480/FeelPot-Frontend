8b065d2ad2389ad59df56d8723c259dcf0b83ee9
silbioa480 <silbioa480@gmail.com>
2022-02-11 22:14:04 +0900

    add RecoilRoot in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,19 @@ import App from "./App";
 import { theme } from "./theme";
 
 import "bootstrap/dist/css/bootstrap.css";
+import { RecoilRoot } from "recoil";
 
 const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
 );
