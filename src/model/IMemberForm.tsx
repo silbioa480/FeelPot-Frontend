a435188d8aa3fcaf27ee06bbc2f6a0dce323bc7b
silbioa480 <silbioa480@gmail.com>
2022-02-12 14:38:37 +0900

    remove Member.tsx class & create IMemberForm.tsx

---
+++ src/model/IMemberForm.tsx
@@ -0,0 +1,12 @@
+interface IMemberForm {
+  id: string;
+  password: string;
+  passwordConfirm: string;
+  name: string;
+  birth: Date;
+  gender: string;
+  email: string;
+  phoneNumber: string;
+}
+
+export default IMemberForm;
