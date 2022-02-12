e420aa087c070d52aefc7f1b19c514d09bbcdd85
silbioa480 <silbioa480@gmail.com>
2022-02-12 15:46:05 +0900

    make Log In page with react-hook-from

--- src/routes/LogIn.tsx
+++ src/routes/LogIn.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import { Alert } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
 import {
   FormContainer,
   FormInput,
@@ -10,89 +9,94 @@ import {
   FormTitle,
   SubmitBtn,
 } from "../css/styledForm";
+import { useForm } from "react-hook-form";
+import { useRecoilValue } from "recoil";
+import { saltKey } from "../atoms";
+import IMemberForm from "../model/IMemberForm";
+import AlertSpan from "../components/AlertSpan";
+import MemberService from "../services/MemberService";
+import IMember from "../model/IMember";
 
 function LogIn() {
-  const [id, setId] = useState("");
-  const [pw, setPw] = useState("");
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<IMemberForm>();
 
-  const [isWarning, setIsWarning] = useState(false);
-  const [warningMsg, setWarningMsg] = useState("");
-  const [warningPosition, setWarningPosition] = useState("");
+  const history = useHistory();
+  const salt = useRecoilValue(saltKey);
 
-  const handleChangeId = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setId(event.currentTarget.value);
-  };
-  const handleChangePw = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPw(event.currentTarget.value);
-  };
+  // Click submit button, onValid is called by handleSubmit.
+  const onValid = async ({ id, password }: IMemberForm) => {
+    // Find member object by id.
+    let member: IMember;
+    try {
+      member = await MemberService.getMemberById(id).then((res) => res.data);
+    } catch (err) {
+      setError(
+        "id",
+        { message: "등록되지 않은 아이디입니다." },
+        { shouldFocus: true }
+      );
 
-  const handleSubmit = (event: React.FormEvent<HTMLElement>) => {
-    event.preventDefault();
-
-    if (!id) {
-      setWarningPosition("-65%");
-      setWarningMsg("아이디를 입력하세요.");
-      setIsWarning(true);
-      setTimeout(() => setIsWarning(false), 2000);
-      document.getElementById("id")?.focus();
-
-      return false;
-    } else if (!pw) {
-      setWarningPosition("-48%");
-      setWarningMsg("비밀번호를 입력하세요.");
-      setIsWarning(true);
-      setTimeout(() => setIsWarning(false), 2000);
-      document.getElementById("pw")?.focus();
-
-      return false;
+      return;
     }
 
-    setId("");
-    setPw("");
+    // Decrypt password and compare input password.
+    let CryptoJS = require("crypto-js");
+    let decrypted = CryptoJS.AES.decrypt(member.password, salt).toString(
+      CryptoJS.enc.Utf8
+    );
+    if (decrypted !== password) {
+      setError(
+        "password",
+        { message: "잘못된 비밀번호입니다." },
+        { shouldFocus: true }
+      );
 
-    alert("로그인 완료!");
+      return;
+    }
+
+    // If all process was completed, move to Home router.
+    history.push("/1");
   };
 
   return (
     <>
       <FormContainer>
-        <FormShape onSubmit={handleSubmit}>
-          <FormTitle>Login</FormTitle>
+        <FormShape onSubmit={handleSubmit(onValid)} style={{ height: "auto" }}>
+          <FormTitle>Log In</FormTitle>
 
           <FormLine>
             <FormLabelP htmlFor="id">ID</FormLabelP>
             <FormInput
               type="text"
-              id="id"
-              value={id}
-              onChange={handleChangeId}
+              {...register("id", { required: "아이디를 입력하세요." })}
             />
           </FormLine>
+          {errors?.id?.message ? <AlertSpan msg={errors.id.message} /> : ""}
 
           <FormLine>
             <FormLabelP htmlFor="pw">PW</FormLabelP>
             <FormInput
               type="password"
               autoComplete="on"
-              id="pw"
-              value={pw}
-              onChange={handleChangePw}
+              {...register("password", { required: "비밀번호를 입력하세요." })}
             />
           </FormLine>
+          {errors?.password?.message ? (
+            <AlertSpan msg={errors.password.message} />
+          ) : (
+            ""
+          )}
 
           <SubmitBtn value="LOG IN" />
 
           <div style={{ textAlign: "center" }}>
             Don't you have ID? <Link to="/signUp">sign up</Link>
           </div>
-
-          <Alert
-            show={isWarning}
-            variant={"danger"}
-            style={{ top: `${warningPosition}` }}
-          >
-            {warningMsg}
-          </Alert>
         </FormShape>
       </FormContainer>
     </>
