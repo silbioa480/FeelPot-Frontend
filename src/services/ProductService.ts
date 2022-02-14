20890baf080d71b1eedcf7f37c1014c6b59372c9
silbioa480 <silbioa480@gmail.com>
2022-02-14 16:57:18 +0900

    create ProductService.ts

---
+++ src/services/ProductService.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosResponse } from "axios";
+import IProduct from "../interface/IProduct";
+
+const PRODUCT_API_BASE_URL = "http://localhost:8080/api/products";
+
+class ProductService {
+  getProducts() {
+    return axios.get<IProduct[]>(PRODUCT_API_BASE_URL);
+  }
+
+  createProduct(product: IProduct) {
+    return axios.post<IProduct>(PRODUCT_API_BASE_URL);
+  }
+
+  getProductById(productId: IProduct["id"]): Promise<AxiosResponse<IProduct>> {
+    return axios.get<IProduct>(PRODUCT_API_BASE_URL + "/" + productId);
+  }
+
+  updateProduct(product: IProduct, productId: IProduct["id"]): Promise<AxiosResponse<IProduct>> {
+    return axios.put<IProduct>(PRODUCT_API_BASE_URL + "/" + productId, product);
+  }
+
+  deleteProduct(productId: IProduct["id"]) {
+    return axios.delete<IProduct>(PRODUCT_API_BASE_URL + "/" + productId);
+  }
+}
+
+export default new ProductService();
