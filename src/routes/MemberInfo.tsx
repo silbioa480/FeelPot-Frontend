4f4a56faa585dff277e2cc2c00c4af29d01c2357
silbioa480 <silbioa480@gmail.com>
2022-02-14 09:59:26 +0900

    modified Member Info page to table

--- src/routes/MemberInfo.tsx
+++ src/routes/MemberInfo.tsx
@@ -1,5 +1,6 @@
 import { useRecoilValue } from "recoil";
 import { loggedMemberAtom } from "../atoms";
+import { Table } from "react-bootstrap";
 
 function MemberInfo() {
   const loggedMember = useRecoilValue(loggedMemberAtom);
@@ -14,15 +15,47 @@ function MemberInfo() {
     "일";
 
   return (
-    <div>
-      <div>환영합니다. {loggedMember.id} 님</div>
-      <div>이름: {loggedMember.name}</div>
-      <div>성별: {loggedMember.isMale ? "남성" : "여성"}</div>
-      <div>주소: {loggedMember.address}</div>
-      <div>생년월일: {birthStr}</div>
-      <div>이메일: {loggedMember.email}</div>
-      <div>전화번호: {loggedMember.phoneNumber}</div>
-    </div>
+    <Table striped bordered hover style={{ width: "90%" }}>
+      <thead>
+        <tr>
+          <th colSpan={2} style={{ textAlign: "center", fontWeight: "bold" }}>
+            환영합니다. {loggedMember.id} 님
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>이름</td>
+          <td>{loggedMember.name}</td>
+        </tr>
+
+        <tr>
+          <td>성별</td>
+          <td>{loggedMember.isMale ? "남성" : "여성"}</td>
+        </tr>
+
+        <tr>
+          <td>주소</td>
+          <td>{loggedMember.address}</td>
+        </tr>
+
+        <tr>
+          <td>생년월일</td>
+          <td>{birthStr}</td>
+        </tr>
+
+        <tr>
+          <td>이메일</td>
+          <td>{loggedMember.email}</td>
+        </tr>
+
+        <tr>
+          <td>전화번호</td>
+          <td>전화번호: {loggedMember.phoneNumber}</td>
+        </tr>
+      </tbody>
+    </Table>
   );
 }
 
