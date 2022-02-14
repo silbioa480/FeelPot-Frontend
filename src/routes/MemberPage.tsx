ba9afb5d5184884f71d6cf0314ed06fd416f516f
silbioa480 <silbioa480@gmail.com>
2022-02-14 10:57:44 +0900

    modified menu box to ListGroup

--- src/routes/MemberPage.tsx
+++ src/routes/MemberPage.tsx
@@ -2,41 +2,75 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { loggedMemberAtom } from "../atoms";
-import styled from "styled-components";
+import { ListGroup } from "react-bootstrap";
 
 type Props = {
   children: React.ReactNode;
 };
-
-const MenuBox = styled.div`
-  border: 1px solid;
-  height: 50px;
-  text-align: center;
-  padding: 15px 0;
-`;
 
 function MemberPage({ children }: Props) {
   const loggedMember = useRecoilValue(loggedMemberAtom);
 
   return (
     <div style={{ display: "flex" }}>
-      <div style={{ width: "30%", flexDirection: "row" }}>
-        <Link to={"/member/" + loggedMember.id}>
-          <MenuBox>회원 정보</MenuBox>
-        </Link>
+      <ListGroup as="ul" style={{ width: "30%" }}>
+        <ListGroup.Item as="li">
+          <Link
+            to={"/member/" + loggedMember.id}
+            style={{
+              display: "inline-block",
+              width: "100%",
+              padding: "10px 0",
+              textAlign: "center",
+            }}
+          >
+            회원 정보
+          </Link>
+        </ListGroup.Item>
 
-        <Link to={"/member/update/" + loggedMember.id}>
-          <MenuBox>회원 정보 수정</MenuBox>
-        </Link>
+        <ListGroup.Item as="li">
+          <Link
+            to={"/member/update/" + loggedMember.id}
+            style={{
+              display: "inline-block",
+              width: "100%",
+              padding: "10px 0",
+              textAlign: "center",
+            }}
+          >
+            회원 정보 수정
+          </Link>
+        </ListGroup.Item>
 
-        <Link to="/member/cart">
-          <MenuBox>장바구니</MenuBox>
-        </Link>
+        <ListGroup.Item as="li">
+          <Link
+            to="/member/cart"
+            style={{
+              display: "inline-block",
+              width: "100%",
+              padding: "10px 0",
+              textAlign: "center",
+            }}
+          >
+            장바구니
+          </Link>
+        </ListGroup.Item>
 
-        <Link to={"/member/order/" + loggedMember.id}>
-          <MenuBox>주문내역</MenuBox>
-        </Link>
-      </div>
+        <ListGroup.Item as="li">
+          <Link
+            to={"/member/order/" + loggedMember.id}
+            style={{
+              display: "inline-block",
+              width: "100%",
+              padding: "10px 0",
+              textAlign: "center",
+            }}
+          >
+            주문내역
+          </Link>
+        </ListGroup.Item>
+      </ListGroup>
+
       <div style={{ width: "70%", display: "flex", justifyContent: "center" }}>
         {children}
       </div>
